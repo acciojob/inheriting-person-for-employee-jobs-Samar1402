b3824bad4bc547dc760cbb435f6fe7976017cf78
Samarjeet Kumar <124017057+Samar1402@users.noreply.github.com>
2024-09-20 09:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+	constructor(name, age) {
+		this._name=name;
+		this._age=age;
+	}
+	get name(){
+		return this._name;
+	}
+	get age(){
+		return this._age;
+	}
+	printPersonDetails(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+
+class Employee extends Person{
+	constructor(name, age,jobTitle) {
+		this._name=name;
+		this._age=age;
+		this._jobTitle=jobTitle
+	}
+	get name(){
+		return this._name;
+	}
+	get age(){
+		return this._age;
+	}
+	get jobTitle(){
+		return this._jobTitle
+	}
+	printEmployeeDetails(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old`)
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
